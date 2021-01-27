ccd2e0c57d1e2edbee840f54313d7eac6f9a2bd8
winnie-byun <winnie.byun@groundx.xyz>
2021-01-27 13:11:14 +0900

    truffle works with klaytn wallet

--- packages/protocol/truffle-config.js
+++ packages/protocol/truffle-config.js
@@ -1,11 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
+
+const privateKey = "0x8a1b191dd20f4e0636f828bb8bab202a6df55d79a852a3416bd862b9a0fd4ebf";
+
 module.exports = {
     networks: {
-        development: {
-            host: "127.0.0.1",
-            port: 8545, // ganache
-            gasPrice: 0,
-            network_id: "*", // Match any network id
-            websockets: true,
+        // development: {
+        //   host: "localhost",
+        //   port: 8545,
+        //   network_id: "*" // Match any network id
+        // },
+        baobab: {
+            provider: () => new HDWalletProvider(privateKey, "https://api.baobab.klaytn.net:8651"),
+            network_id: '1001', //Klaytn baobab testnet's network id
+            gas: '8500000',
+            gasPrice: null
+        },
+        cypress: {
+            provider: () => new HDWalletProvider(privateKey, "https://api.cypress.klaytn.net:8651"),
+            network_id: '8217', //Klaytn mainnet's network id
+            gas: '8500000',
+            gasPrice: null
         }
     },
     compilers: {
